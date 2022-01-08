ae85c44b1f97351d56d99dc65077556e82daeb78
Y4sperJmih <95161707+Y4sperJmih@users.noreply.github.com>
2022-01-08 20:55:43 +0300

    Update typedip.js

--- js/typedip.js
+++ js/typedip.js
@@ -1,7 +1,7 @@
 $.ajax({
     url: "https://ident.me", success: function (result) {
         new Typed('.typed', {
-            strings: ["^300cd /main/Y4sper/^300\n`~/main/Y4sper/ $ `^600scan^200 `" + result + "`^300\n`" + "Scanning " + result + "...`^3000\n`Sucess!`^500"],
+            strings: ["^300cd /main/Y4sper/^300\n`~/main/Y4sper/ $ `^600scan^200 `" + result + "`^300\n`" + "Scanning " + result + "...`^3000\n`Success!`^500"],
             typeSpeed: 65,
             backSpeed: 0,
             cursorChar: '_',
