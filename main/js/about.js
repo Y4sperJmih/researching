4288ab202001f55b32bc1e0d3e420c55e8e7eb66
Y4sperJmih <95161707+Y4sperJmih@users.noreply.github.com>
2022-01-11 23:05:44 +0300

    Add files via upload

--- main/js/about.js
+++ main/js/about.js
@@ -1,12 +1,15 @@
 let section = document.querySelector(".first")
 let about = document.querySelector(".about")
+let video = document.querySelector("video")
 
 section.addEventListener("click", () => {
     if (!about.classList.contains("about_in")) {
         about.classList.remove("about_out")
         about.classList.add("about_in");
+        video.play()
     } else {
         about.classList.remove("about_in");
         about.classList.add("about_out");
+        video.pause()
     }
 })
