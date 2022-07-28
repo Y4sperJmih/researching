49c4912b550e5ecb41c0d410ec60a099a408044b
Y4sperJmih <95161707+Y4sperJmih@users.noreply.github.com>
2022-07-28 21:04:53 +0300

    Delete main directory

--- main/js/preloader.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("load", function () {
-    document.querySelector('.preloader > div');
-    setTimeout(() => {
-      document.querySelector('.preloader').classList.add('loaded');
-    }, 1);
-})
